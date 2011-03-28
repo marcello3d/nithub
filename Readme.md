aab296a611a57d42b5f80999905e67a43269f08b
Marcello Bastea-Forte <marcello@marcello-dev.local>
2011-03-27 23:33:16 -0400

    initial commit!

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Nithub
+======
+Nithub mashes up the npm and github apis to organize and rank npm packages.
+
+Introduction
+------------
+It's hard to figure out what's good, so this is one attempt to improve that.
---
+++ server.js
@@ -0,0 +1,10 @@
+require('./lib/scanner')
+
+var log = require('./lib/shared').log('server.js')
+
+var app = require('./lib/www')
+
+app.listen(8000, function() {
+    log.info("Started http://"+app.address().address+":"+app.address().port)
+})
+
